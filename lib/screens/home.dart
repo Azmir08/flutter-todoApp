ec7a689f2fe004514083b2925dd07153cb92894b
azmirHP <azmirmahni08@gmail.com>
2024-01-11 22:49:10 +0800

    AppBar & Search Bar

---
+++ lib/screens/home.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          children: [
+            searchBox(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //Search Box widget
+  Widget searchBox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: const TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
+      ),
+    );
+  }
+
+  //Extract method to create a method name AppBar
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0, //to remove shadow in appbar
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        const Icon(
+          Icons.menu,
+          color: tdBlack,
+          size: 30,
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Image.asset(
+            'assets/images/coffee-story.jpg',
+            height: 50,
+            width: 50,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ]),
+    );
+  }
+}
